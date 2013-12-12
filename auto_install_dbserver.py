7518b57971b9e5ae1a7acc28c069e5acb62e40fb
John Lu <lushilonger@foxmail.com>
2013-12-12 14:38:30 +0800

    Update auto_install_dbserver.py

--- auto_install_dbserver.py
+++ auto_install_dbserver.py
@@ -2,8 +2,7 @@
 #-*-coding=utf-8
 # 
 
-
-import os, sys, re
+import os, sys, re, locale, shutil
 from ftplib import FTP
 import socket
 import commands
@@ -13,32 +12,45 @@ http_port = 80
 https_port = 443
 mysql_port = 3306
 mysql_passwd = "dingjia"
-ftp_ip = 
-ftp_user = 
-ftp_passwd = 
-ftp_dir = 
+ftp_ip = "192.168.88.10"
+ftp_user = "scutech"
+ftp_passwd = "dingjia"
+ftp_dir = "/ftp_product_installer/wddps/2013/2.2.17/"
+
+
+# This function try to connect the FTP
+def ConnectFtp():
+    try:
+        socket.gethostbyname("baidu.com")
+    except:
+        print "Can not connect to internet !!!"
+        sys.exit(0)
+
+    try:
+        ftp = FTP(ftp_ip)
+    except:
+        print "Cna not connect to Ftp Server !!!"
+        sys.exit(0)
+    try:
+        ftp.login(ftp_user, ftp_passwd)
+    except:
+        print "Can not login to Ftp Server !!!"
+        sys.exit(0)
+    try:
+        ftp.cwd(ftp_dir)
+    except:
+        print "Can not open Ftp directory !!!"
+        sys.exit(0)
+
+    return ftp
 
 
 # This function get the last version package
 def GetLastVsersion():
 
-    try:
-        socket.gethostbyname("baidu.com")
-    except:
-        return 1
-
-    ftp = FTP(ftp_ip)
-    try:
-        ftp.login(ftp_user, ftp_passwd)
-    except:
-        return 0
-    try:
-        ftp.cwd(ftp_dir)
-    except Error:
-        return 0
-
-    package_list = ftp.nlst()
-    pattern = r'dbackup-server-2.2-([0-9]{5}).([0-9]{2}).noarch.el6.run'
+    searchftp = ConnectFtp()
+    package_list = searchftp.nlst()
+    pattern = r'dbackup-server-2.2-([0-9]{4,5}).([0-9]{2}).noarch.el6.run'
     handle = re.compile(pattern)
     package_version = 0
     last_version_package = ''
@@ -53,36 +65,27 @@ def GetLastVsersion():
                 last_version_package = i
 
     if package_version == 0:
-        return 0
+        print "Can not match the package......"
+        sys.exit(0)
     else:
         return last_version_package
 
-    ftp.quit()
+    searchftp.quit()
 
 
 # define function download last version of Dbackup Agent Package from Ftp
 def AutoDownPackage(package_name):
 
-    try:
-        socket.gethostbyname("baidu.com")
-    except:
-        return 1
-    
-    ftp = FTP(ftp_ip)
-    try:
-        ftp.login(ftp_user, ftp_passwd)
-    except:
-        return 0
-
-    try:
-        ftp.cwd(ftp_dir)
-    except Error:
-        return 0
-
+    downloadftp = ConnectFtp() 
     localfile = "/home/serverpackage/" + package_name
-    ftp.retrbinary('RETR ' + package_name, open(localfile, 'wb').write)
+    try:
+        downloadftp.retrbinary('RETR ' + package_name, open(localfile, 'wb').write)
+        print "Download Dbackup Server successful !!!"
+    except:
+        print "Download Dbackup Server fail........"
+        sys.exit(0)
 
-    ftp.quit()
+    downloadftp.quit()
 
 
 # define function autoinstall DbackupServer
@@ -109,12 +112,15 @@ def AutoInstallServer(package_name):
 # define function autouninstall DbackupServer
 def AutoUninstallServer():
 
-    os.system('rpm -e dbackup-server')
-    os.system('rpm -e dbackup-lic')
-    os.system('rpm -e scutech-php')
-    os.system('rpm -e scutech-apache')
-    os.system('rpm -e scutech-mysql')
-    return 0
+    try:
+        os.system('rpm -e dbackup-server')
+        os.system('rpm -e dbackup-lic')
+        os.system('rpm -e scutech-php')
+        os.system('rpm -e scutech-apache')
+        os.system('rpm -e scutech-mysql')
+        return 0
+    except:
+        return 1
 
 
 # define function check if install Dbserver
@@ -129,34 +135,20 @@ if __name__ == '__main__':
     os.system('rm -rf /home/serverpackage')
     os.system('mkdir /home/serverpackage')
     last_package = GetLastVsersion()
-    if last_package == 1:
-        print "Can not connect Internet !!!"
-        sys.exit(0)
-    elif last_package == 0:
-        print "Can not connect to FTP Server !!!"
-        sys.exit(0)
     get_current = CheckInfo() 
-    newestversion = re.findall(r'(?<=-).{12}?(?=\.)', last_package)
+    newestversion = re.findall(r'(?<=-).{11,12}?(?=\.)', last_package)
     if get_current == "package dbackup-server is not installed":
         print "This System has not installed Dbackup Server!"
         print "The last version is [ %s ]" % newestversion[0]
         ips = raw_input('Press "ENTER" to install newest Dbackup Server: ')
         print "Downloading Dbackup Server......"
         downloads = AutoDownPackage(last_package)
-        if downloads == 1:
-            print "Can not connect Internet !!!"
-            sys.exit(0)
-        elif downloads == 0:
-            print "Can not connect to FTP Server !!!"
-            sys.exit(0)
-        else:
-            print "Download Dbackup Server successful !!!"
         if AutoInstallServer(last_package) == 0:
             print "Install Dbackup Server successful !!!"
         else:
             print "Install Dbackup Server fail......"
     else:
-        currentversion = re.findall(r'(?<=-).{12}?(?=\.)', get_current)
+        currentversion = re.findall(r'(?<=-).{11,12}?(?=\.)', get_current)
         print "There is aready installed [ %s ]" % currentversion[0]
         print "So the newest version is [ %s ]" % newestversion[0]
         print "You can do:"
@@ -184,14 +176,6 @@ if __name__ == '__main__':
             if get_input == 2:
                 print "Downloading Dbackup Server......"
                 downloads = AutoDownPackage(last_package)
-                if downloads == 1:
-                    print "Can not connect Internet !!!"
-                    sys.exit(0)
-                elif downloads == 0:
-                    print "Can not connect to FTP Server !!!"
-                    sys.exit(0)
-                else:
-                    print "Download Dbackup Server successful !!!"
                 print "Installing Dbackup Server......"
                 if AutoInstallServer(last_package) == 0:
                     print "Dbackup Server install successful !!!"
